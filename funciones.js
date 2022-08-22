e02b201b35660757e82aa7a5ee6a599bc1e6b01c
M. Obregón <mopsabadel@gmail.com>
2022-08-22 13:06:44 -0300

    Modificación de array

--- funciones.js
+++ funciones.js
@@ -8,33 +8,45 @@ let salarioAbril=parseInt(prompt("Ingrese su sueldo líquido de Abril"));
 let salarioMayo=parseInt(prompt("Ingrese su sueldo líquido de Mayo"));
 let salarioPrimerSemestre=(salarioDiciembre + salarioEnero + salarioFebrero + salarioMarzo + salarioAbril + salarioMayo)
 
-let valorAguinaldo1=(salarioPrimerSemestre /12 )  
-alert("El aguinaldo de Junio es " + valorAguinaldo1);
+let aguinaldoJunio=(salarioPrimerSemestre /12 )  
+alert("El aguinaldo de Junio es " + aguinaldoJunio);
 
-const aguinaldoJunio=[salarioDiciembre, salarioEnero, salarioFebrero, 
+
+//USO DE ARRAYS PARA PRIMER AGUINALDO
+const arraySueldos1=[salarioDiciembre, salarioEnero, salarioFebrero, 
 						salarioMarzo, salarioAbril, salarioMayo]
+console.log("La cantidad de meses ingresados son " + arraySueldos1.length);
+console.log(arraySueldos1.includes(salarioMarzo))
 
-console.log("La cantidad de meses ingresados son " + aguinaldoJunio.length);
-console.log(aguinaldoJunio.includes(salarioMarzo))
+//Consulta si quiere calcular aguinaldo diciembre
+let consulta = prompt("¿Desea calcular el aguinaldo de Diciembre?")
 
-//Array de aguinaldo Diciembre 
-let salarioJunio=parseInt(prompt("Ingrese su sueldo líquido de Junio"));
-let salarioJulio=parseInt(prompt("Ingrese su sueldo líquido de Julio"));
-let salarioAgosto=parseInt(prompt("Ingrese su sueldo líquido de Agosto"));
-let salarioSeptiembre=parseInt(prompt("Ingrese su sueldo líquido de Septiembre"));
-let salarioOctubre=parseInt(prompt("Ingrese su sueldo líquido de Octubre"));
-let salarioNoviembre=parseInt(prompt("Ingrese su sueldo líquido de Noviembre"));
-let salarioSegundoSemestre=(salarioJunio + salarioJulio + salarioAgosto + 
-					salarioSeptiembre + salarioOctubre + salarioNoviembre)
+if (consulta=="si") {
+	let salarioJunio=parseInt(prompt("Ingrese su sueldo líquido de Junio"));
+	let salarioJulio=parseInt(prompt("Ingrese su sueldo líquido de Julio"));
+	let salarioAgosto=parseInt(prompt("Ingrese su sueldo líquido de Agosto"));
+	let salarioSeptiembre=parseInt(prompt("Ingrese su sueldo líquido de Septiembre"));
+	let salarioOctubre=parseInt(prompt("Ingrese su sueldo líquido de Octubre"));
+	let salarioNoviembre=parseInt(prompt("Ingrese su sueldo líquido de Noviembre"));
+	let salarioSegundoSemestre=(salarioJunio + salarioJulio + salarioAgosto + 
+				salarioSeptiembre + salarioOctubre + salarioNoviembre)
 
-let valorAguinaldo2=(salarioSegundoSemestre /12 )  
-alert("El aguinaldo de Diciembre es " + valorAguinaldo2);
+	let aguinaldoDiciembre=(salarioSegundoSemestre /12 )  
+	alert("El aguinaldo de Diciembre es " + aguinaldoDiciembre);
+	alert("Gracias por usar este servicio.")
+	const arraySueldos2=[salarioJunio, salarioJulio, salarioAgosto, 
+				salarioSeptiembre, salarioOctubre, salarioNoviembre]
+	//USO DE ARRAYS PARA SEGUNDO AGUINALDO
+	console.log("La cantidad de meses ingresados son " + aguinaldoDiciembre.length);
+	console.log(aguinaldoDiciembre.includes(salarioMarzo))
 
-const aguinaldoDiciembre=[salarioJunio, salarioJulio, salarioAgosto, 
-						salarioSeptiembre, salarioOctubre, salarioNoviembre]
 
-console.log("La cantidad de meses ingresados son " + aguinaldoDiciembre.length);
-console.log(aguinaldoDiciembre.includes(salarioMarzo))
+} else {
+	alert("Gracias por usar este servicio.")
+}
+
+
+
 
 
 
